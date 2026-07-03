a4d31aa09ab9d0d6513d2def831be48bbe471c45
Navy <lengrathanaknavy@gmail.com>
2026-07-03 15:49:25 +0700

    Refactor sign-up and create events pages: enhance layout, improve styling, and add preview functionality

---
+++ create_events.js
@@ -0,0 +1,27 @@
+function updatePreview(){
+    document.getElementById("previewTitle").textContent= document.getElementById("title").value || "--";
+    document.getElementById("previewCategory").textContent= document.getElementById("category").value || "--";
+    document.getElementById("previewDescription").textContent= document.getElementById("description").value || "--";
+    document.getElementById("previewDate").textContent= document.getElementById("eventDate").value || "--";
+    document.getElementById("previewTime").textContent= document.getElementById("eventTime").value || "--";
+    document.getElementById("previewVenue").textContent= document.getElementById("venue").value || "--";
+    document.getElementById("previewPrice").textContent= document.getElementById("price").value || "--";
+    document.getElementById("previewTickets").textContent= document.getElementById("tickets").value || "--";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const formInputs = document.querySelectorAll("input, textarea, select");
+    formInputs.forEach(input => {
+        input.addEventListener("input", updatePreview);
+        input.addEventListener("change", updatePreview);
+    });
+    updatePreview(); 
+});
+
+
+    
+
+document.getElementById("publish-btn").addEventListener("click", alertPublish);
+function alertPublish() {
+    alert("Your event is in the review process. You will be notified once it is approved and published."); 
+}
